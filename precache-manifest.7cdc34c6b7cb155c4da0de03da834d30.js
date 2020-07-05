7d774579bfa659a94920efb244194e0a0ccff21b
Joonas <joonas@shopz.com>
2020-07-05 16:58:29 +0300

    Updates

---
+++ precache-manifest.7cdc34c6b7cb155c4da0de03da834d30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e79f8fbf6771f2ad69108a5d2a4345e",
+    "url": "/sarv/index.html"
+  },
+  {
+    "revision": "be73dde9d6efe856d23c",
+    "url": "/sarv/static/css/main.c1dc799a.chunk.css"
+  },
+  {
+    "revision": "0c39dc00cab6d65549b6",
+    "url": "/sarv/static/js/2.edf23932.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sarv/static/js/2.edf23932.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be73dde9d6efe856d23c",
+    "url": "/sarv/static/js/main.3389bc3c.chunk.js"
+  },
+  {
+    "revision": "3681c84c60772b274edd",
+    "url": "/sarv/static/js/runtime-main.057222c7.js"
+  }
+]);
